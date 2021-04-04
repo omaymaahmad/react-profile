d1f81d281f0bfc57482d9e891bb90ef8444f482f
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 15:33:16 +0100

    feat: added download button for cv

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { GrCloudDownload } from 'react-icons/gr';
+import cv from "../Assets/Omayma-Ahmad-Resume.pdf"
 
 function CV () {
     return (
+        <>
         <h1>cv</h1>
+        <Button variant="dark" ><GrCloudDownload/> <a href={cv} download>Download CV</a></Button>
+        </>
     )
 }
 
