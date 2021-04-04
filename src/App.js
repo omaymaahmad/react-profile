4643a03a6f2e0b807c22ec088375a0ba5a7b980c
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 14:43:11 +0100

    feat: routing for porjects page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from './Components/NavBar';
 import AboutMe from './Components/AboutMe';
+import Projects from './Components/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     < Router>
       < NavBar />
       < Route exact path="/" component={AboutMe}/>
+      < Route exact path="/projects" component={Projects}/>
     </Router>
     </>
   );
