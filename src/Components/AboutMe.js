657139455c54ad612eb193f130391d1a1c0f4f65
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 15:20:28 +0100

    feat: started implementing carousel

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import photo from "../Assets/Images/9B3194C8-37CD-4F56-B2C7-3962B1C5425A.jpeg"
 
 function AboutMe () {
     return (
-        <h1>about me</h1>
-    )
+    <>
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={require("../Assets/Images/9B3194C8-37CD-4F56-B2C7-3962B1C5425A.jpeg")}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={photo}
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=Third slide&bg=20232a"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+   </> )
 }
 
 export default AboutMe;
