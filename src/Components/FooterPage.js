bfc2c2aa94af5ff89c9ec3da13e26b4d83bdbf7e
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 22:38:00 +0100

    feat: edited footer and added icon

--- src/Components/FooterPage.js
+++ src/Components/FooterPage.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { MDBContainer, MDBFooter } from "mdbreact";
+import { IoCloudyNightOutline } from "react-icons/io5";
 
 const FooterPage = () => {
   return (
     <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-   
-      </MDBContainer>
+      <MDBContainer fluid className="text-center text-md-left"></MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; omayma {new Date().getFullYear()} <IoCloudyNightOutline />
         </MDBContainer>
       </div>
     </MDBFooter>
   );
-}
+};
 
 export default FooterPage;
