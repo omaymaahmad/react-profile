04d74a833916bd47e36d1c1b20dc761271b084a0
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 21:11:05 +0100

    feat: cloud icon to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Navbar, Nav} from "react-bootstrap";
+import { IoCloudyNightOutline } from 'react-icons/io5';
+
 
 function NavBar () {
     return (
         <Navbar bg="light" variant="light">
-        <Navbar.Brand href="/">omayma</Navbar.Brand>
+        <Navbar.Brand href="/"> <IoCloudyNightOutline/> omayma </Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">About Me</Nav.Link>
           <Nav.Link href="/projects">Projects</Nav.Link>
