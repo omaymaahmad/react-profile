c39e803c08533176436a3569e76f246ec9f41ad5
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 22:12:18 +0100

    feat: added buttons for linkedin and github page on cv component'

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { GrCloudDownload } from 'react-icons/gr';
-import cv from "../Assets/Omayma-Ahmad-Resume.pdf"
+import React from "react";
+import { Button, Jumbotron, Container } from "react-bootstrap";
+import { GrCloudDownload } from "react-icons/gr";
+import cv from "../Assets/Omayma-Ahmad-Resume.pdf";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
-function CV () {
-    return (
-        <>
-        <h1>cv</h1>
-        <Button variant="info" ><GrCloudDownload/> <a href={cv} download>Download CV</a></Button>
-        </>
-    )
+function CV() {
+  return (
+    <>
+      <body>
+        <Jumbotron fluid>
+          <Container className="cvContainer">
+            <h1>CV & other links</h1>
+            <p>For any further information use the buttons below.</p>
+            <p>
+              <Button variant="secondary">
+                <GrCloudDownload />{" "}
+                <a href={cv} download>
+                  Download CV
+                </a>
+              </Button>
+            </p>
+            <br></br>
+            <p>
+              <Button variant="info">
+                <AiFillLinkedin />{" "}
+                <a href="https://www.linkedin.com/in/omayma-ahmad-14922a1b8/">
+                  LinkedIn
+                </a>
+              </Button>
+            </p>
+            <br></br>
+            <p>
+              <Button variant="info">
+                <AiFillGithub />{" "}
+                <a href="https://github.com/omaymaahmad">
+                  GitHub
+                </a>
+              </Button>
+            </p>
+          </Container>
+        </Jumbotron>
+      </body>
+    </>
+  );
 }
 
 export default CV;
