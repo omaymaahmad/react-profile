6500f58a42cab20ed8237e42328e7dfb3d6bef30
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-04 14:33:14 +0100

    feat: set up navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
+    <>
+    < NavBar />
     <Header />
+    </>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Navbar, Nav} from "react-bootstrap";
+
+function NavBar () {
+    return (
+        <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">omayma</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">About Me</Nav.Link>
+          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/cv">CV</Nav.Link>
+        </Nav>
+      </Navbar>
+    )
+}
+export default NavBar;
